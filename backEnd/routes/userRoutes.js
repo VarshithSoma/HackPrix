c50bf8f918d75e627e39ef48571bc438207d8775
varshithkumarsoma@gmail.com <varshithkumarsoma@gmail.com>
2024-06-08 19:24:25 +0530

    added max backend

---
+++ backEnd/routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const Router = express.Router();
+Router.post("/signup", authController.signup);
+Router.post("/login", authController.login);
+Router.get("/logout", authController.logOut);
+
+// Router.patch("/update/:id", authController.protect,doctorController.update);
+// Router.get(
+//   "/me",
+//   authController.protect,
+//   authController.getMe,
+//   authController.getUser
+// );
+// Router.get("/", doctorController.getAllDoctors);
+//   .post(doctorController.createUser);
+// Router.route("/:id")
+//   .get(doctorController.getUser)
+//   .patch(doctorController.updateUser)
+//   .delete(doctorController.deleteUser);
+module.exports = Router;
